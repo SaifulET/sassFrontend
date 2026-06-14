51eead6dd8eceb6e0c24c022383bc36df30bd285
Saiful <si912999@gmail.com>
2026-06-15 00:09:02 +0600

    integrate AutomationsMailPage routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import SalonSettingsPage from "../components/SalonSettingsPage";
 import SalonActivityPage from "../components/SalonActivityPage";
 import SalonSupportPage from "../components/SalonSupportPage";
 import SalonAutomationsPage from "../components/SalonAutomationsPage";
+import AutomationsMailPage from "../components/AutomationsMailPage";
 import WaiversPage from "../components/WaiversPage";
 import SettingsPage from "../components/SettingsPage";
 
@@ -440,6 +441,10 @@ export default function Home() {
       case "billing":
         return (
           <BillingRevenuePage />
+        );
+      case "automations":
+        return (
+          <AutomationsMailPage />
         );
       case "waivers":
         return (
